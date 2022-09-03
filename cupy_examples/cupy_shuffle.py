1fad87bd274f7d176d50a502f5a6dfb3fd753554
magung <mulya.agung@ed.ac.uk>
2022-09-04 00:42:51 +0100

    Benchmarking array shuffling with Cupy and Numba

---
+++ cupy_examples/cupy_shuffle.py
@@ -0,0 +1,6 @@
+import cupy
+
+
+block_size = 4096
+num_snps = 1000000
+
---
+++ cupy_examples/raw_kernels.py
@@ -0,0 +1,24 @@
+from cupyx import jit
+import cupy
+
+
+@jit.rawkernel()
+def elementwise_ols(x, y, b, size):
+    tid = jit.blockIdx.x * jit.blockDim.x + jit.threadIdx.x
+    ntid = jit.gridDim.x * jit.blockDim.x
+
+    for i in range(tid, size, ntid):
+        b[i] = y[i] / x[i]
+        #b[i] = squared_diff(x[i], y[i])
+    #b = cupy.linalg.lstsq(x, y)
+
+
+size = cupy.uint32(2 ** 22)
+x = cupy.random.normal(size=(size,), dtype=cupy.float32)
+y = cupy.random.normal(size=(size,), dtype=cupy.float32)
+b = cupy.empty((size,), dtype=cupy.float32)
+
+elementwise_ols((128, ), (1024, ), (x, y, b, size))
+
+#assert (x == y).all()
+print(b)
